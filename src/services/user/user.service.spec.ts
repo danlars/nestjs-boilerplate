825489dc44f118b1f998e0149f6898ec1359794d
danlars <dal@monsido.com>
2019-07-26 17:44:25 +0200

    Update the user service

--- src/services/user/user.service.spec.ts
+++ src/services/user/user.service.spec.ts
@@ -1,14 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import { UserEntity } from '../../entities/user/user.entity';
-import { DeleteResult } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { MockUserEntity } from '../../../mocks/entities/user/user.entity';
+import { PaginationService } from '../pagination/pagination.service';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useClass: MockUserEntity,
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
@@ -18,64 +26,88 @@ describe('UserService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should return a collection of user entities on getAll', async () => {
-    jest.spyOn(UserEntity, 'find').mockImplementation(() => {
-      return new Promise((resolve) => resolve([]));
+  describe('#getAll', () => {
+    it('should return a collection', async () => {
+      expect(await service.getAll({})).toStrictEqual([]);
     });
-    service.getAll({})
-      .then(
-        (res) => expect(res).toMatchObject([]),
-        () => console.log('Error on getAll'),
-      );
   });
 
-  it('should return a new UserEntity on update', async () => {
-    const updateEntity = new UserEntity();
-    updateEntity.firstname = 'Dan';
-    const findSpy = jest.spyOn(UserEntity, 'findOne').mockImplementation(() => {
-      return new Promise((resolve) => resolve(updateEntity));
+  describe('#get', () => {
+    it('should return a object', async () => {
+      expect(await service.get(1)).toMatchObject({});
     });
-    const saveSpy = jest.spyOn(updateEntity, 'save').mockImplementation(() => {
-      return new Promise((resolve) => resolve(updateEntity));
+  });
+
+  describe('#create', () => {
+    it('should return a object', async () => {
+      const user = {
+        firstname: 'Dan',
+        password: 'test',
+        email: 'dan@test.com',
+      };
+      expect(await service.create(user)).toMatchObject(user);
     });
-    service.update(1, {})
-      .then(
-        (res) => expect(res).toMatchObject(updateEntity),
-        () => console.log('Error on update'),
-      ).finally(() => {
-        findSpy.mockClear();
-        saveSpy.mockClear();
+  });
+
+  describe('#update', () => {
+    it('should return a object without password', async () => {
+      const user = {
+        firstname: 'Dan',
+      };
+      expect(await service.update(1, user)).toMatchObject(user);
+    });
+
+    it('should return a object with password', async () => {
+      const user = {
+        firstname: 'Dan',
+        password: 'test',
+      };
+      expect(await service.update(1, user)).toMatchObject(user);
+    });
+  });
+
+  describe('#destroy', () => {
+    it('should return DeleteResult', async () => {
+      expect(await service.destroy(1)).toBeDefined();
+    });
+  });
+
+  describe('#paginate', () => {
+    const paginationResult = {
+      current_page: 1,
+      last_page: 1,
+      page_size: 10,
+      total: 0,
+      data: [],
+    };
+    let paginateSpy;
+
+    beforeEach(() => {
+      paginateSpy = jest.spyOn(PaginationService, 'paginate').mockImplementation(() => {
+        return new Promise((resolve) => resolve(paginationResult));
       });
+    });
+
+    it('should return PaginationResult, without using search', async () => {
+      const paginate = await service.paginate({ page: 1, page_size: 10, search: '' });
+      expect(paginate).toStrictEqual(paginationResult);
+    });
+
+    it('should return PaginationResult, using search', async () => {
+      const paginate = await service.paginate({ page: 1, page_size: 10, search: 'test' });
+      expect(paginate).toStrictEqual(paginationResult);
+    });
   });
 
-  it('should return DeleteResult on destroy', async () => {
-    const spy = jest.spyOn(UserEntity, 'delete').mockImplementation(() => {
-      return new Promise((resolve) => resolve(new DeleteResult()));
+  describe('#findOneByEmail', () => {
+    it('should return object', async () => {
+      expect(await service.findOneByEmail('test@email.com')).toMatchObject({});
     });
-    service.destroy(1)
-      .then(
-        (res) => expect(res).toBeDefined(),
-        () => console.log('Error on Destroy'),
-      ).finally(() => spy);
   });
 
-  it('should return UserEntity on findOneByEmail method', async () => {
-    const spy = jest.spyOn(UserEntity, 'findOne').mockImplementation(() => {
-      return new Promise((resolve) => resolve(new UserEntity()));
+  describe('#findOneByToken', () => {
+    it('should return object', async () => {
+      expect(await service.findOneByToken('token')).toMatchObject({});
     });
-    expect(service.findOneByEmail(''))
-      .resolves
-      .toStrictEqual(expect.any(UserEntity));
-    spy.mockClear();
-  });
-
-  it('should return UserEntity on findOneByToken method', async () => {
-    const spy = jest.spyOn(UserEntity, 'findOne').mockImplementation(() => {
-      return new Promise((resolve) => resolve(new UserEntity()));
-    });
-    expect(service.findOneByToken(''))
-      .resolves
-      .toStrictEqual(expect.any(UserEntity));
-    spy.mockClear();
   });
 });
